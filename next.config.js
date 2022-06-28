37062cf0f2f1773148bf8a852c57c4fbb5a41a76
Shrivatsa Bhat <52356082+shrivatsabhat@users.noreply.github.com>
2022-06-28 20:27:03 +0530

    style: base styles added

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
   },
   reactStrictMode: true,
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, 'src', 'styles')],
   },
 }
 
